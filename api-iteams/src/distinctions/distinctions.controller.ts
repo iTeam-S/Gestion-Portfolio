f71afc726bee1d8f298738ddb05387d980f2b6a5
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-06-29 20:14:09 +0300

    [ADD] distinctions ge ...

--- api-iteams/src/distinctions/distinctions.controller.ts
+++ api-iteams/src/distinctions/distinctions.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { DistinctionsService } from './distinctions.service';
 
 @Controller('distinctions')
-export class DistinctionsController {}
+export class DistinctionsController {
+    constructor(private readonly distinctionsService: DistinctionsService) {}
+
+    @UseGuards(AuthGuard('jwtMembre'))
+    @Get()
+    async getDistinctions(@Request() req: any) {
+        const data = { id: parseInt(req.user.id) };
+        return await this.distinctionsService.findOne(data);
+    }
+}
--- api-iteams/src/distinctions/distinctions.service.ts
+++ api-iteams/src/distinctions/distinctions.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Distinctions, Membre } from 'src/output';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class DistinctionsService {}
+export class DistinctionsService {
+    constructor(
+        @InjectRepository(Distinctions)
+        private distinctionsRepository: Repository<Distinctions>
+    ) {}
+
+    async findOne(donnees: { id: number }): Promise<Distinctions[]> {
+        return await this.distinctionsRepository
+            .createQueryBuilder("d")
+            .select([
+                "d.id AS id", "d.organisateur AS organisateur", 
+                "d.annee AS annee", "d.type AS type", "d.description AS description", 
+                "d.ordre AS ordre", "m.prenom_usuel AS prenom_usuel"
+            ])
+            .innerJoin(Membre, "m", "d.id_membre=m.id")
+            .where("m.id=:identifiant", { identifiant: donnees.id })
+            .getRawMany();
+    }
+}
