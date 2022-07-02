6b067eea3a5dc6de11d00030a821c6ce48c9345e
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-07-02 19:44:01 +0300

    [ADD] swagger...

--- api-iteams/src/main.ts
+++ api-iteams/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('iTeam-$')
+    .setDescription('Interface de documentation API iTeam-$ Community')
+    .setVersion('1.0')
+    .addTag('iTeam-$')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
