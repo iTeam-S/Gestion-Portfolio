c3f67cf7d9ec86e798f4ec1ffda9269452c75098
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-07-02 10:52:10 +0300

    [UPDATE] formations module...

--- api-iteams/src/formations/formations.module.ts
+++ api-iteams/src/formations/formations.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FormationsService } from './formations.service';
 import { FormationsController } from './formations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Formations } from 'src/output';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Formations])
+  ],
   providers: [FormationsService],
   controllers: [FormationsController]
 })
