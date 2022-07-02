ad85eb12f17cbb1e0b74cc4c2408ab9601cfba72
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-07-02 20:05:41 +0300

    change port...

--- api-iteams/src/main.ts
+++ api-iteams/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(3031);
 }
 bootstrap();
