60104b7d2a886251385f461943f40b7b87df793e
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-07-02 10:55:48 +0300

    [UPDATE] distinctions module...

--- api-iteams/src/distinctions/distinctions.module.ts
+++ api-iteams/src/distinctions/distinctions.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Distinctions } from 'src/output';
 import { DistinctionsController } from './distinctions.controller';
 import { DistinctionsService } from './distinctions.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Distinctions])
+  ],
   controllers: [DistinctionsController],
   providers: [DistinctionsService]
 })
