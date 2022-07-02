fcde6f000f1db3b3970f6c2517a86b8b5ea331d2
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-07-02 10:54:01 +0300

    [UPDATE] fonction module...

--- api-iteams/src/fonction/fonction.module.ts
+++ api-iteams/src/fonction/fonction.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Fonction } from 'src/output';
 import { FonctionController } from './fonction.controller';
 import { FonctionService } from './fonction.service';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Fonction])
+  ],
   controllers: [FonctionController],
   providers: [FonctionService]
 })
